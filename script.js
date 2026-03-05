3f924ce3c9d18b9d1eb8fcf9c6570ab1c759a052
zyarifakbar7-art <zyarifakbar7@gmail.com>
2026-03-05 12:37:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -587,10 +587,10 @@ scene.add(planet);
 
 //Ganti Deskripsi Planet
 const ringTexts = [
-  'Galaxy of love From ...',//untuk deskripsi planet layer 1
-  "I love you",//untuk deskripsi planet layer 2
-  "♡Happy Girlfriend day♡",//untuk deskripsi planet layer 3
-  "01/08/2025",//untuk deskripsi planet layer 4
+  'HAPPY BRITHDAY ',//untuk deskripsi planet layer 1
+  "MARISAA",//untuk deskripsi planet layer 2
+  "A SPECAL DAY FOR YOU",//untuk deskripsi planet layer 3
+  "03/23/2026",//untuk deskripsi planet layer 4
   ...(window.dataCCD && window.dataCCD.data.ringTexts ? window.dataCCD.data.ringTexts : [])
 ];
 
